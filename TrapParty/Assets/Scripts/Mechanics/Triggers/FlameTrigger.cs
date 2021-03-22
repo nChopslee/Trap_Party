00075ebd55af6433bd33b5ea1c10131b30e538af
Albert <albertvlambert@gmail.com>
2021-03-22 13:07:28 -0500

    Lava Level Design

---
+++ TrapParty/Assets/Scripts/Mechanics/Triggers/FlameTrigger.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlameTrigger : MonoBehaviour
+{
+    
+    public ParticleSystem Fire;
+    public ParticleSystem Fire1;
+    public ParticleSystem Fire2;
+    public ParticleSystem Fire3;
+    public ParticleSystem Fire4;
+    public ParticleSystem Fire5;
+    public ParticleSystem Fire6;
+    public ParticleSystem Fire7;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+           
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Fire1.maxParticless
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            Fire.SetActive(true);
+            Fire1.SetActive(true);
+            Fire2.SetActive(true);
+            Fire3.SetActive(true);
+            Fire4.SetActive(true);
+            Fire5.SetActive(true);
+            Fire6.SetActive(true);
+            Fire7.SetActive(true);
+            Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            Fire.SetActive(false);
+            Fire1.SetActive(false);
+            Fire2.SetActive(false);
+            Fire3.SetActive(false);
+            Fire4.SetActive(false);
+            Fire5.SetActive(false);
+            Fire6.SetActive(false);
+            Fire7.SetActive(false);
+
+        }
+    }
+}
---
+++ TrapParty/Assets/Scripts/Mechanics/Triggers/LavaTrigger.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LavaTrigger : MonoBehaviour
+{
+    
+    public GameObject lava;
+    public GameObject lava1;
+    public GameObject lava2;
+    public GameObject lava3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            lava.SetActive(true);
+            lava1.SetActive(true);
+            lava2.SetActive(true);
+            lava3.SetActive(true);
+            Debug.Log(col.gameObject.name + " : " + gameObject.name + " : " + Time.time);
+        }
+    }
+    void OnTriggerExit2D(Collider2D col)
+    {
+
+        if (col.gameObject.tag == "Player")
+        {
+            lava.SetActive(false);
+            lava1.SetActive(false);
+            lava2.SetActive(false);
+            lava3.SetActive(false);
+
+        }
+    }
+}
