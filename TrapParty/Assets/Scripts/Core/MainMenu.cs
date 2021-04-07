044427ede61cd3cc51deadf10aaa1e2d12367291
LicenseToKILL52 <80073469+LicenseToKILL52@users.noreply.github.com>
2021-04-07 18:09:57 -0500

    Added UI Scripts

---
+++ TrapParty/Assets/Scripts/Core/MainMenu.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ExitButton() {
+	Application.Quit();
+	Debug.Log("You have exited out of the game");
+    }
+
+    public void StartButton() {
+	SceneManager.LoadScene("Trap_Party_0.2_Jungle");
+    }
+}
