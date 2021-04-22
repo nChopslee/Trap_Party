a373406f8c8368eee58b50a907be4e4929c1da57
LicenseToKILL52 <80073469+LicenseToKILL52@users.noreply.github.com>
2021-04-22 17:13:39 -0500

    Added More Button Scripts

--- TrapParty/Assets/Scripts/Core/MainMenu.cs
+++ TrapParty/Assets/Scripts/Core/MainMenu.cs
@@ -6,12 +6,19 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     public void ExitButton() {
-	    Application.Quit();
-	    Debug.Log("You have exited out of the game");
+	Application.Quit();
+	Debug.Log("You have exited out of the game");
+    }
+
+    public void SettingsButton() {
+	SceneManager.LoadScene("Settings");
+    }
+
+    public void CreditsButton() {
+	SceneManager.LoadScene("Credits");
     }
 
     public void StartButton() {
-	    SceneManager.LoadScene("Trap_Party_0.2_Jungle");
-       // SceneManager.LoadScene("AdditiveScene", LoadSceneMode.Additive);
+	SceneManager.LoadScene("Trap_Party_0.2_Jungle");
     }
 }
